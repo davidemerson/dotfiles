7777563c2e359da8387d67e0d6a5e9a099e67509
David Emerson <david@refmark.net>
2021-02-10 11:26:56 -0500

    Update execute.sh

--- execute.sh
+++ execute.sh
@@ -3,6 +3,7 @@
 # Let's get the latest dotfiles from github, clean out the old states, and run salt.
 
 git pull
-rm -rf /srv/salt/*
+rm -rf /srv/salt/
+mkdir /srv/salt
 cp -R salt/* /srv/salt/
 salt-call --local state.highstate
