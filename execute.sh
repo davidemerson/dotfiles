75f7b7cb81e40e1da0bd1ed61b75f6658f4a5ccb
David Emerson <d@nnix.com>
2025-01-24 12:21:24 -0500

    expanded provisioning script

--- execute.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/ksh
-# runs salt after copying some files to the right places.
-cp minion /etc/salt/minion
-rm -rf /srv/salt/
-mkdir -p /srv/salt
-cp -R salt/* /srv/salt/
-salt-call --local state.highstate
-fc-cache
-ntpdate 0.pool.ntp.org
